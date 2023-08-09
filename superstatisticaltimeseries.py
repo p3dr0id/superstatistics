7520f4ca77111f6b2edac32d12361fce851f604f
Pedro D. Pinto <100048804+p3dr0id@users.noreply.github.com>
2023-08-09 11:02:43 +0000

    load data

--- superstatisticaltimeseries.py
+++ superstatisticaltimeseries.py
@@ -1,1 +1,18 @@
+import time
+from scipy.stats import kurtosis
+import numpy as np
+import multiprocessing as mp
+import matplotlib.pyplot as plt
+
+nprocs = mp.cpu_count()
+pool = mp.Pool(processes=nprocs)
+print(f'Number of CPU cores: {nprocs}')
+
+# Load Soybean historical prices (macrotrends)
+dados_soja = np.loadtxt('soybean-prices-historical-chart-data.csv', 
+                        delimiter=",", 
+                        dtype=str, 
+                        skiprows=1)
+
+print(dados_soja)
 
