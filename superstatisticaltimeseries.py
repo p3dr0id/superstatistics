5f9f41bb76fc1c9e7def430eb48f2c3a16f0ecb0
Pedro D. Pinto <100048804+p3dr0id@users.noreply.github.com>
2023-08-09 17:22:11 +0000

    graph

--- superstatisticaltimeseries.py
+++ superstatisticaltimeseries.py
@@ -1,7 +1,7 @@
 import time
+import multiprocessing as mp
 from scipy.stats import kurtosis
 import numpy as np
-import multiprocessing as mp
 import matplotlib.pyplot as plt
 
 nprocs = mp.cpu_count()
@@ -9,10 +9,13 @@ pool = mp.Pool(processes=nprocs)
 print(f'Number of CPU cores: {nprocs}')
 
 # Load Soybean historical prices (macrotrends)
-dados_soja = np.loadtxt('soybean-prices-historical-chart-data.csv', 
-                        delimiter=",", 
-                        dtype=str, 
+dados_soja = np.loadtxt('soybean-prices-historical-chart-data.csv',
+                        delimiter=",",
+                        dtype=str,
                         skiprows=1)
 
-print(dados_soja)
+precos_soja = dados_soja[:, 1].astype(float)
+retornos_soja = np.diff(np.log(precos_soja))
 
+#plt.plot(retornos_soja)
+#plt.savefig('retornos')
